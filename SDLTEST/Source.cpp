15a077c9cda3358705c86429b66a11cfac1c5baf
sherzock <jacobo.galofre@gmail.com>
2018-03-05 20:30:25 +0100

    .

--- SDLTEST/Source.cpp
+++ SDLTEST/Source.cpp
@@ -12,8 +12,18 @@
 static const int width = 800;
 static const int height = 600;
 
+void bullet(SDL_Renderer *renderer, int x0, int y0) {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_Rect reco = { x0, y0, 50, 20 };
+	SDL_RenderFillRect(renderer, &reco);
+}
+
 int main(int argc, char **argv)
 {
+	//position init
+	int x = 288;
+	int y = 208;
+
 	// Initialize SDL
 	SDL_Init(SDL_INIT_VIDEO);
 
@@ -27,33 +37,60 @@ int main(int argc, char **argv)
 	SDL_Event close;
 
 	while (!quit) {
-		while (SDL_PollEvent(&close)!= 0) {
+		/*while (SDL_PollEvent(&close)!= 0) {
 			if (close.type == SDL_QUIT) {
 				quit = true;
+			}*/
+		SDL_WaitEvent(&close);
+
+
+
+		// Clear screen with blue
+		SDL_SetRenderDrawColor(renderer, 100, 120, 10, 255);
+		SDL_RenderClear(renderer);
+
+		// Draw
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_Rect rect = { x, y, 200, 200 };
+		SDL_RenderFillRect(renderer, &rect);
+
+
+
+		switch (close.type)
+		{
+		case SDL_QUIT:
+			quit = true;
+			break;
+
+
+		case SDL_KEYDOWN:
+			switch (close.key.keysym.sym) {
+			case SDLK_UP:
+				y = y - 10 ;
+				break;
+			case SDLK_DOWN:
+				y = y + 10;
+				break;
+			case SDLK_LEFT:
+				x = x - 10;
+				break;
+			case SDLK_RIGHT:
+				x = x + 10;
+				break;
+			case SDLK_KP_SPACE:
+				bullet(renderer,x,y);
+				break;
 			}
 		}
 
-
-		
-			// Clear screen with blue
-			SDL_SetRenderDrawColor(renderer, 100, 120, 10, 255);
-			SDL_RenderClear(renderer);
-
-			// Draw
-			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-			SDL_Rect rect = { 50, 50, 200, 200 };
-			SDL_RenderFillRect(renderer, &rect);
+		// Show what was drawn
+		SDL_RenderPresent(renderer);
 
 
+		//delay
+		SDL_Delay(1);
 
-			// Show what was drawn
-			SDL_RenderPresent(renderer);
 
-
-			//delay
-			SDL_Delay(5);
-
-		
 	}
 
 
