da61c05220963175aadc44b437bbdbe42032c0fc
sherzock <jacobo.galofre@gmail.com>
2018-03-06 10:23:25 +0100

    flow correction + image libraries (made in class)
    made code correction so the movement was more fluid and the code was more compact and added SDL image libraries

--- SDLTEST/Source.cpp
+++ SDLTEST/Source.cpp
@@ -1,159 +1,159 @@
 #include "SDL/include/SDL.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include "SDL_Image/include/SDL_image.h"
 
 #pragma comment(lib, "SDL/libx86/SDL2.lib")
 #pragma comment(lib, "SDL/libx86/SDL2main.lib")
+#pragma comment(lib, "SDL_Image/libx86/SDL2_image.lib")
 
 
+//screen size
+const int SCREEN_WIDTH = 700;
+const int SCREEN_HEIGHT = 700;
 
-const int SCREEN_WIDTH = 1000;
-const int SCREEN_HEIGHT = 1000;
-
-bool key_up = NULL;
-bool key_down = NULL;
-bool key_right = NULL;
-bool key_left = NULL;
-bool key_space = NULL;
+bool keyup = 0;
+bool keydown = 0;
+bool keyright = 0;
+bool keyleft = 0;
+bool keyspace = 0;
 
 int main(int argc, char* argv[]) {
 
 	//The window we'll be rendering to
 	SDL_Window* window = NULL;
-
+	
+	
 	//The surface contained by the window
 	SDL_Surface* screenSurface = NULL;
-
+	
+	
 	//Initialize SDL
 	SDL_Init(SDL_INIT_VIDEO);
-
-	//Create window  "Name at the top", x position, y position, windows width, windows height, SDL_WINDOW_SHOWN makes sure the window is shown when it is created
-	window = SDL_CreateWindow("Projecte 1", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-
+	
+	
+	//Create window 
+	window = SDL_CreateWindow("test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	
+	
 	//Create render
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	
+	
 	bool quit = false;
-	SDL_Event close;
-
-	int size_x = 100;
-	int size_y = 100;
+	SDL_Event event;
+	
+	
+	int size_x = 40;
+	int size_y = 40;
 	int pos_x = (SCREEN_WIDTH / 2) - (size_x / 2);
 	int pos_y = (SCREEN_HEIGHT / 2) - (size_y / 2);
-	SDL_Rect rect2;
+	SDL_Rect rectangle2;
+
 
 	while (!quit) {
-		while (SDL_PollEvent(&close) != 0) {
-			//User requests quit
-			if (close.type == SDL_QUIT) {
-				quit = true;
-			}
-			if (close.type == SDL_KEYUP) {
-				switch (close.key.keysym.sym) {
-				case SDLK_UP:
-					key_up = false;
-					break;
-
-				case SDLK_DOWN:
-					key_down = false;
-					break;
-
-				case SDLK_LEFT:
-					key_left = false;
-					break;
-
-				case SDLK_RIGHT:
-					key_right = false;
-					break;
-
-				case SDLK_SPACE:
-					key_space = false;
-					break;
-				}
-			}
-
-			if (close.type == SDL_KEYDOWN) {
-				switch (close.key.keysym.sym) {
-				case SDLK_UP:
-					key_up = true;
-					break;
-
-				case SDLK_DOWN:
-					key_down = true;
-					break;
-
-				case SDLK_LEFT:
-					key_left = true;
-					break;
-
-				case SDLK_RIGHT:
-					key_right = true;
-					break;
-
-				case SDLK_SPACE:
-					key_space = true;
-					break;
-				}
-			}
-
-			if (key_up) { pos_y -= 5; }
-
-			if (key_down) { pos_y += 5; }
-
-			if (key_right) { pos_x += 5; }
-
-			if (key_left) { pos_x -= 5; }
-
-			if (key_space) {
-				rect2 = { pos_x + size_x, pos_y + (size_y / 2), 50, 10 };
-			}
-
-			if (pos_x >= SCREEN_WIDTH - size_x) {
-				pos_x = SCREEN_WIDTH - size_x;
-			}
-
-			if (pos_x <= 0) {
-				pos_x = 0;
-			}
-
-			if (pos_y >= SCREEN_HEIGHT - size_y) {
-				pos_y = SCREEN_HEIGHT - size_y;
-			}
-
-			if (pos_y <= 0) {
-				pos_y = 0;
-			}
-		}
-
-
+		
+		
 		//Clear screen with blue
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
-
-		//Draw
+		
+		
+		//Draw rectangle
 		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-		SDL_Rect rect = { pos_x, pos_y, size_x, size_y };
-		SDL_RenderFillRect(renderer, &rect);
+		SDL_Rect rectangle = { pos_x, pos_y, size_x, size_y };
+		SDL_RenderFillRect(renderer, &rectangle);
 
-		rect2.x += 2;
-		if (rect2.x <= SCREEN_WIDTH) {
-			SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-			SDL_RenderFillRect(renderer, &rect2);
-			SDL_Delay(1);
+
+		while (SDL_PollEvent(&event) != 0) {
+			
+			
+			//User requests quit
+			if (event.type == SDL_QUIT) {
+				
+				quit = true;
+
+			}
+			//setting keyup
+			
+			if (event.type == SDL_KEYUP) {
+				
+				switch (event.key.keysym.sym) {
+				
+				case SDLK_UP:
+					keyup = false;
+					break;
+				
+				case SDLK_DOWN:
+					keydown = false;
+					break;
+				
+				case SDLK_LEFT:
+					keyleft = false;
+					break;
+				
+				case SDLK_RIGHT:
+					keyright = false;
+					break;
+				
+				case SDLK_SPACE:
+					keyspace = false;
+					break;
+				}
+			}
+			//setting keydown
+			if (event.type == SDL_KEYDOWN) {
+				
+				switch (event.key.keysym.sym) {
+				
+				case SDLK_UP:
+					keyup = true;
+					break;
+				
+				case SDLK_DOWN:
+					keydown = true;
+					break;
+				
+				case SDLK_LEFT:
+					keyleft = true;
+					break;
+				
+				case SDLK_RIGHT:
+					keyright = true;
+					break;
+				
+				case SDLK_SPACE:
+					keyspace = true;
+					break;
+				}
+			}
 		}
 
+		//setting movement
+		if (keyup && (rectangle.y >= 0)) { pos_y -= 1; }
+
+		if (keydown && (rectangle.y <= SCREEN_HEIGHT - size_y)) { pos_y += 1; }
+
+		if (keyright && (rectangle.x <= SCREEN_WIDTH - size_x)) { pos_x += 1; }
+
+		if (keyleft && (rectangle.x >= 0)) { pos_x -= 1; }
+
+		if (keyspace) { rectangle2 = { pos_x + size_x, pos_y + (size_y / 2)-4, 20, 10 }; }
+
+		rectangle2.x +=2;
+		if (rectangle2.x <= SCREEN_WIDTH) {
+			SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+			SDL_RenderFillRect(renderer, &rectangle2);
+		}
+		
+		SDL_Delay(1);
+		
 		//Show what was drawn
 		SDL_RenderPresent(renderer);
 	}
-
-	/*
+	
 	//Destroy window
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
-	*/
-
-	return 0;
+	
+	return 0; 
 }
